5904daa9985ee6ab7b68c15c410f44670a7fdb96
Connor-Kubas <conman1507@gmail.com>
2023-05-11 03:04:49 -0500

    Worked on card stacks

---
+++ vsverse/static/js/script.js
@@ -0,0 +1,15 @@
+// window.addEventListener('DOMContentLoaded', function() {
+//     const columns = document.getElementsByClassName('image-column');
+  
+//     for (let i = 0; i < columns.length; i++) {
+//       const column = columns[i];
+//       const images = column.getElementsByClassName('overlay-image');
+//       let topOffset = 0;
+  
+//       for (let j = 0; j < images.length; j++) {
+//         images[j].style.top = topOffset + '%';
+//         topOffset += 20; // Adjust the value to determine the spacing between images
+//       }
+//     }
+//   });
+  
