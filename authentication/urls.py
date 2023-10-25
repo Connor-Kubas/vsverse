f876a8ecbc099c50126787270e659058d4e7f1e8
Connor-Kubas <conman1507@gmail.com>
2023-10-24 20:35:01 -0500

    Improved feature.

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login', views.login, name='login'),
+    path('login-user', views.login_user, name='login-user'),
+
     path('sign-up', views.sign_up, name="sign-up")
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def login(request):
+    print('applesauce')
     return render(request, 'login.html', context={})
+
+def login_user(request):
+    print('eeeeeeeeeeeeeeeeeeeeeeeeeeeee')
+    return render(request, 'index.html')
 
 def sign_up(request):
     return render(request, 'sign-up.html', context={})
