4ab5ae38d848b30a97d6a897fd83f84043684584
Connor-Kubas <conman1507@gmail.com>
2024-05-05 02:19:45 -0500

    Working on stats.

--- deck_editor/view_directory/DisplayView.py
+++ deck_editor/view_directory/DisplayView.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render
 from core.models import Decks
 from core.models import DeckCards
-from core.models import Data
 
 def deck(request, deck_id, display_method="stack"):
     deck_cards = DeckCards.objects.filter(deck_id=deck_id).order_by('card__cost')
-    deck = Decks.objects.filter(id=deck_id)  
+    deck = Decks.objects.get(id=deck_id)
 
-    card_ids_and_quantities = deck_cards.values_list('card_id', 'quantity')
-
+    card_types = {}
     total_cards = 0
-    for card, (_, quantity) in zip(deck_cards, card_ids_and_quantities):
-        card.quantity = quantity
+
+    non_character_count = 0
+    character_count = 0
+
+    for deck_card in deck_cards:
+        quantity = deck_card.quantity
         total_cards += quantity
+        
+        non_character_count += deck_card.quantity if deck_card.card.type != 'Character' else 0
+        character_count += deck_card.quantity if deck_card.card.type == 'Character' else 0
+        card_type = deck_card.card.type
+        card_types[card_type] = card_types.get(card_type, 0) + quantity
 
     context = {
         'deck_cards': deck_cards,
@@ -20,12 +27,9 @@ def deck(request, deck_id, display_method="stack"):
         'deck_id': deck_id,
         'display_method': display_method,
         'total_cards': total_cards,
+        'card_types': card_types,
+        'character_count': character_count,
+        'non_character_count': non_character_count,
     }
 
     return render(request, 'deck.html', context)
-
-# def getUUIDFromCard(card):
-#     # print(card)
-#     data = Data.objects.filter(title=card.title, version=card.version)
-
-#     return data.uuid
