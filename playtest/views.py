768ac440e696ee0756de391c2d5af286cb30d5e6
Connor-Kubas <conman1507@gmail.com>
2024-05-10 01:31:36 -0500

    Got the play area and searching working pretty well.

--- playtest/views.py
+++ playtest/views.py
@@ -19,16 +19,36 @@ def show(request, deck_id):
     # data = Data.objects.filter(title=card.title, version=card.version, type=card.type).exclude(type='Planet')[:1].get()
 
     data = []
-    for card in cards:
-        data_object = Data.objects.filter(title=card.title, version=card.version, type=card.type).exclude(type='Planet')[:1]
-        # print(data_object.values()[0])
-        data.append(data_object.values()[0])
-    # print(data)
+    # for card in cards:
+    #     data_object = Data.objects.filter(title=card.title, version=card.version, type=card.type).exclude(type='Planet')[:1]
+    #     # print(data_object.values()[0])
+        
+    #     data.append(data_object.values()[0])
+
+    # This adds the correct amount of cards according to the quantity in the deck.
+    for deck_card in deck_cards:
+        data_object = Data.objects.filter(title=deck_card.card.title, version=deck_card.card.version, type=deck_card.card.type).exclude(type='Planet')[:1]
+        t = data_object.values()[0]
+        for i in range(deck_card.quantity):
+            # This adds a unique identifier to each card.
+            t['identifier'] = str(i) + t['uuid']
+            data.append(t)
+
     card_json = json.dumps(data)
 
     context = {
         'cards': data,
+        'uuid_list': data,
         'card_json': card_json
     }
 
     return render(request, 'playtest/templates/index.html', context)
+
+# def show(request, deck_id):
+#     deck_cards = DeckCards.objects.filter(deck_id=deck_id)
+
+#     uuids = {}
+#     for deck_card, i in deck_cards
+#         uuids[i] = 
+
+#     return 
